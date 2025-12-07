d5c93e45a8aca867ce7c5fe4fd4356f338f4e4b1
Shinjini Chakraborty <shinjini.chakraborty2023@iem.edu.in>
2025-12-07 16:25:42 +0530

    frontend loading perfectly

---
+++ neutralino-app/.tmp/resources/_next/static/chunks/0937e82ab0d16191.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/a41dbe67f1447a5e.js",
+  "static/chunks/7ecb8c89062dc03f.js",
+  "static/chunks/turbopack-b0ba6ca43a1507de.js"
+])
---
+++ neutralino-app/.tmp/resources/_next/static/chunks/4f386f78a108b75a.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/b32851cd04a9508b.js",
+  "static/chunks/7ecb8c89062dc03f.js",
+  "static/chunks/turbopack-37579ebf8af645a9.js"
+])
